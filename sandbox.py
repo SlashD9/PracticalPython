c75b135a2c236c4c7bb3ff88463b611b07eb86c3
Colin Daly <colin.p.s.daly@gmail.com>
2018-05-12 10:37:57 +0000

    Sandbox.py: Made some modifications to file

--- sandbox.py
+++ sandbox.py
@@ -1,5 +1,10 @@
+# Setting some Global variables
 qs = int
 line_question = ""
+ans = int
+line_answer = ""
+
+# This functions gets the question based on then paramenter provided
 def get_question(qs):
     f = open('./text/question.txt')
     for x, line_question in enumerate(f):
@@ -7,10 +12,7 @@ def get_question(qs):
             return (line_question)
     f.close()
 
-
-
-ans = int
-line_answer = ""
+# This functions gets the answer based on then paramenter provided
 def get_answer(ans):
     f = open('./text/answer.txt')
     for x, line_answer in enumerate(f):
