18d1d5b73a8426b11dd6ca1114d7a36cfe02c3a1
Colin Daly <colin.p.s.daly@gmail.com>
2018-05-12 10:40:39 +0000

    run.py: added functions from sandbox to run.py

--- run.py
+++ run.py
@@ -3,6 +3,27 @@ import os
 from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
+# Setting some Global variables
+qs = int
+line_question = ""
+ans = int
+line_answer = ""
+
+# This functions gets the question based on then paramenter provided
+def get_question(qs):
+    f = open('./text/question.txt')
+    for x, line_question in enumerate(f):
+        if x == qs:
+            return (line_question)
+    f.close()
+
+# This functions gets the answer based on then paramenter provided
+def get_answer(ans):
+    f = open('./text/answer.txt')
+    for x, line_answer in enumerate(f):
+        if x == ans:
+            return (line_answer)
+    f.close()
 
 
 @app.route('/')
