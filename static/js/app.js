64af461cb1975cfc2080fb749bd5acef916bdd5e
Colin Daly <colin.p.s.daly@gmail.com>
2018-05-19 06:42:26 +0000

    Tried to add Javascript function to show wrong answers, this does not work as the page reloads

--- static/js/app.js
+++ static/js/app.js
@@ -6,3 +6,24 @@ function show_answer() {
     document.getElementById("answer").style.display = "block";
 }
  */
+
+var x = 0;
+var array = Array();
+
+function add_element_to_array() {
+    array[x] = document.getElementById("answer").value;
+    console.log(array[x])
+    x++;
+    document.getElementById("answer").value = "";
+    display_array();
+};
+
+function display_array() {
+    var e = "<hr/>";   
+    
+    for (var y=0; y<array.length; y++)
+    {
+     e += "Element " + y + " = " + array[y] + "<br/>";
+    }
+    document.getElementById("Result").innerHTML = e;
+};
